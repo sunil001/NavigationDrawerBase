46fd58ebbf53fc95817daa4dc77a89ddeb111f89
hlsunil <sunil.h.l@hubzu.com>
2018-02-07 14:13:53 +0530

    update

---
+++ app/src/main/java/subra/tech/labs/fragment/HomeFragment.java
@@ -0,0 +1,29 @@
+package subra.tech.labs.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import subra.tech.labs.R;
+
+/**
+ * Created by Admin on 3/13/2017.
+ */
+public class HomeFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_maps, container, false);
+        return v;
+    }
+}
+
